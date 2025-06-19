2b595a0776508555d21cbb557d31801784dd6515
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-06-19 21:49:12 +0530

    complete code for rsa hybrid approach

--- RSASignandEncrypt.java
+++ RSASignandEncrypt.java
@@ -2,10 +2,10 @@ import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.Signature;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -18,51 +18,73 @@ public class RSASignandEncrypt {
         KeyPairGenerator key_gen = KeyPairGenerator.getInstance("RSA");
         key_gen.initialize(2048);
 
+        // Sending from Alice to Bob!
         KeyPair alice_key_pair = key_gen.generateKeyPair();
-
         PrivateKey alice_pvt_key = alice_key_pair.getPrivate();
         PublicKey alice_pub_key = alice_key_pair.getPublic();
 
         KeyPair bob_key_pair = key_gen.generateKeyPair();
-
         PrivateKey bob_pvt_key = bob_key_pair.getPrivate();
         PublicKey bob_pub_key = bob_key_pair.getPublic();
 
-        // System.out.println("Private Key: " + new String(pvt_key.getEncoded()));
-        // System.out.println("Public Key: " + new String(pub_key.getEncoded()));
-
-
+        // ----------------Alice----------------------
         String message = "Sample secret message";
+        System.out.println();
         System.out.println("Plain Text: " + message);
+        System.out.println();
         
         Cipher cipher = Cipher.getInstance("RSA");
         byte[] byte_msg = message.getBytes(StandardCharsets.UTF_8);
         
+        // Alice hashes the message with SHA-256
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        byte[] message_hash = sha256.digest(byte_msg);
+        String hash_string = Base64.getEncoder().encodeToString(message_hash);
+        System.out.println("=============Alice================");
+        System.out.println("SHA256 hash of the message: " + hash_string);
+        System.out.println();
+
+        // Alice signs the hash with Alice's private key
         cipher.init(Cipher.ENCRYPT_MODE, alice_pvt_key);
-        byte[] signed_text = cipher.doFinal(byte_msg);
+        byte[] signed_hash = cipher.doFinal(message_hash);
+        String signed_hash_string = Base64.getEncoder().encodeToString(signed_hash);
+        System.out.println("Signed hash: " + signed_hash_string);
+        System.out.println();
 
+        // Alice encrypts the message with Bob's public key
         cipher.init(Cipher.ENCRYPT_MODE, bob_pub_key);
-        byte[] cipher_text = cipher.doFinal(signed_text);
+        byte[] cipher_text = cipher.doFinal(byte_msg);
 
-
-        System.out.println("Cipher Text: " + new String(cipher_text));
         String encoded_msg = Base64.getEncoder().encodeToString(cipher_text);
-
         System.out.println("Cipher Text: " + encoded_msg);
+        System.out.println();
 
-        // Transmit
+        // <<<<<<<<<<<<<<Transmit>>>>>>>>>>>>>>>>>>>>>>>>
         // Receiver side
-        cipher.init(Cipher.DECRYPT_MODE, );
+        // ------------------Bob--------------------------
 
-        byte[] decrypted_text = cipher.doFinal(cipher_text);
-        String plain_text = new String(decrypted_text, StandardCharsets.UTF_8);
+        // Bob decrypts the message
+        cipher.init(Cipher.DECRYPT_MODE, bob_pvt_key);
+        byte[] decrypted_message = cipher.doFinal(cipher_text);
+        String dec_plain_text = new String(decrypted_message, StandardCharsets.UTF_8);
+        System.out.println("==============Bob=================");
+        System.out.println("Decrypted Plain Text: " + dec_plain_text);
+        System.out.println();
+
+        // Bob verifies the signature
+        byte[] hash_bob = sha256.digest(decrypted_message);
+        String hash_bob_string = Base64.getEncoder().encodeToString(hash_bob);
+        System.out.println("Hash calculated by Bob: " + hash_bob_string);
+        System.out.println();
         
-        System.out.println("Decrypted Text (Plain Text): " + plain_text);
+        cipher.init(Cipher.DECRYPT_MODE, alice_pub_key);
+        byte[] decrypted_signature = cipher.doFinal(signed_hash);
+        String decrypted_signature_string = Base64.getEncoder().encodeToString(decrypted_signature);
+        System.out.println("Decrypted signature: " + decrypted_signature_string);
+        System.out.println();
 
-
-        //
-
-        //Signature publicSignature = Signature.getInstance("SHA256withRSA");
+        boolean valid = MessageDigest.isEqual(hash_bob, decrypted_signature);
+        System.out.println("Signature valid? : " + valid);
         
     }
 }
