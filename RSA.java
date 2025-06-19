535b3dc070b1a1cc251f6c650d63c0e23005ccc3
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2025-06-19 16:09:47 +0530

    RSA code

---
+++ RSA.java
@@ -0,0 +1,51 @@
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class RSA {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        KeyPairGenerator key_gen = KeyPairGenerator.getInstance("RSA");
+        key_gen.initialize(2048);
+        KeyPair key_pair = key_gen.generateKeyPair();
+
+        PrivateKey pvt_key = key_pair.getPrivate();
+        PublicKey pub_key = key_pair.getPublic();
+
+        System.out.println("Private Key: " + new String(pvt_key.getEncoded()));
+        System.out.println("Public Key: " + new String(pub_key.getEncoded()));
+
+
+        String message = "Sample secret message";
+        System.out.println("Plain Text: " + message);
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, pub_key);
+
+        byte[] byte_msg = message.getBytes(StandardCharsets.UTF_8);
+        byte[] cipher_text = cipher.doFinal(byte_msg);
+
+        System.out.println("Cipher Text: " + new String(cipher_text));
+        String encoded_msg = Base64.getEncoder().encodeToString(cipher_text);
+
+        System.out.println("Cipher Text: " + encoded_msg);
+
+        // Transmit
+        // Receiver side
+        cipher.init(Cipher.DECRYPT_MODE, pvt_key);
+
+        byte[] decrypted_text = cipher.doFinal(cipher_text);
+        String plain_text = new String(decrypted_text, StandardCharsets.UTF_8);
+        
+        System.out.println("Decrypted Text (Plain Text): " + plain_text);
+    }
+}
